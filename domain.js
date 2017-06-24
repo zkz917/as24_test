1dc527099a4d525925ac675b5f3471701efa5814
Kuangzheng Zhou <kuangzhengzhou@Kuangzhengs-MacBook-Air.local>
2017-06-24 10:45:33 -0400

    Add input validation

---
+++ domain.js
@@ -0,0 +1,25 @@
+
+var t = require('tcomb');
+
+// a subtype is a pair (type, predicate)
+// where predicate is a function with signature (x) -> boolean
+
+
+// enum
+var fuel_type = t.enums.of('gasoline diesel');
+
+// a struct is a type containing properties (i.e. a class)
+
+var avertInput = t.struct({ 
+                    title: t.String,
+                    fuel: t.maybe(fuel_type),
+                    price: t.Number,
+                    new: t.Boolean,
+                    mileage: t.Number,
+                    first_registration: t.String
+                    });
+
+
+module.exports = {
+ avertInput: avertInput
+};
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express     =   require("express");
 var app         =   express();
 var bodyParser  =   require("body-parser");
 var router      =   express.Router();
+var t           =   require("tcomb-validation")
+var validation  =   require("./domain")
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({"extended" : false}));
@@ -12,15 +14,14 @@ mongoose.connect('mongodb://localhost:27017/testapp');
 // create instance of Schema
 var mongoSchema =   mongoose.Schema;
 // create schema
-var userSchema  = {
-    title: String,
-    fuel: String,
-    price: Number,
-    new: Boolean,
-    mileage: Number,
-    first_registration: Date
+var userSchema  = { title: String,
+                    fuel: String,
+                    price: Number,
+                    new: Boolean,
+                    mileage: Number,
+                    first_registration: Date
 
-};
+                  };
 
 // create model if not exists.
 var mongoOp = mongoose.model('userLogin',userSchema);
@@ -34,6 +35,7 @@ router.get("/",function(req,res){
 
 
 
+// return all users 
 
 router.route("/users")
     .get(function(req,res){
@@ -53,48 +55,56 @@ router.route("/users")
     });
 
 
+// add advert to db
 
 router.route("/addusers")
     .post(function(req,res){
         var db = new mongoOp();
         var response = {};
         // Add strict validation when you use this in Production.
+        // var input = JSON.parse(req.body);
+        var input = req.body;
+        var result = t.validate(input, validation.avertInput);
+
+        if (result.isValid()) {
+
+                db.title = req.body.title;
+                db.fuel  = req.body.fuel;
+                db.price = req.body.price;
+                db.new   = req.body.new;
+
+                // only when the car is used then add mileage and Reg date to db.
+
+                if (!req.body.new){
+
+                    db.mileage = req.body.mileage;
+
+                    db.first_registration = req.body.first_registration;
+
+                }
 
 
-        db.title = req.body.title;
-        db.fuel  = req.body.fuel;
-        db.price = req.body.price;
-        db.new   = req.body.new;
-
-        // only when the car is used then add mileage and Reg date to db.
-
-        if (!req.body.new){
-
-            db.mileage = req.body.mileage;
-
-            db.first_registration = req.body.fr;
-
-        }
-
-
-
-
-
-
-        db.save(function(err){
-        // save() will run insert() command of MongoDB.
-        // it will add new data in collection.
-            if(err) {
-                response = {"error" : true, "message" : "Error adding data"};
-            } else {
-                response = {"error" : false, "message" : "Data added"};
+                db.save(function(err){
+                // save() will run insert() command of MongoDB.
+                // it will add new data in collection.
+                    if(err) {
+                        response = {"error" :  true, "message" : err.message };
+                    } else {
+                        response = {"error" : false, "message" : "Data added"};
+                    }
+                    res.json(response);
+                });
+            }else {
+                // in result.errors there are details on the validation failure
+                res.status(400).json(result.errors);
             }
-            res.json(response);
-        });
 
 
 
     });
+
+
+// Search by ID
 
 router.route("/users/:id")
     .get(function(req,res){
@@ -102,13 +112,49 @@ router.route("/users/:id")
         mongoOp.findById(req.params.id,function(err,data){
         // This will run Mongo Query to fetch data based on ID.
             if(err) {
-                response = {"error" : true,"message" : "Error fetching data"};
+                response = {"error" : true,"message" : err.message};
             } else {
                 response = {"error" : false,"message" : data};
             }
             res.json(response);
         });
     });
+
+// Update by car advert by id
+
+router.route("/update/:id")
+    .post(function(req,res){
+        var response = {};
+        
+        var new_tile   = req.body.title;
+        // var new_fuel   = req.body.fuel;
+        // var new_price  = req.body.price;
+        // var new_new    = req.body.new;
+        // var new_mileage= req.body.mileage;
+        // var new_fr     = req.body.fr;
+
+        var new_obj    = {title : new_tile,
+                          // fuel  : new_fuel,
+                          // price : new_price,
+                          // new   : new_new,
+                          // mileage: new_mileage,
+                          // first_registration : new_fr
+                         };
+
+
+        
+        mongoOp.findByIdAndUpdate(req.params.id,req.body,function(err,data){
+        // Mongo command to fetch all data from collection.
+            if(err) {
+                response = {"error" : true,"message" : "Error fetching data"};
+            } else {
+                response = {"error" : false,"message" : "updated"};
+                console.log(data);
+            }
+            res.json(response);
+        });
+    });
+
 
 app.use('/',router);
 
